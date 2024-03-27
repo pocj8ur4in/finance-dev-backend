ce2957d435b896d4e9b095d57919c4825b638e09
pocj8ur4in <pocj8ur4in@gmail.com>
2024-03-27 17:44:49 +0900

    init: 루트 컨트롤러 추가

---
+++ api/src/main/java/finance/dev/api/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package finance.dev.api.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
